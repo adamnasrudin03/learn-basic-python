597d71cd304c8cb99a59110f6fd9f2cc8e9f53eb
Adam Nasrudin <adamnasrudin858@gmail.com>
2024-06-18 18:47:22 +0700

    List comprehension

--- 05-list/main.py
+++ 05-list/main.py
@@ -274,3 +274,124 @@ list_2 = [ 'h','z', 'c']
 list_2.sort()
 print(list_2)
 # output ➜ ['c', 'h', 'z']
+
+
+# %% List comprehension
+list_1 = [10, 70, 20]
+list_2 = [e*2 for e in list_1]
+print(list_2)
+# output ➜ [20, 140, 40]
+
+list_1 = [10, 70, 20]
+list_2 = [e*2 for e in list_1 if e > 10]
+print(list_2)
+# output ➜ [20, 140]
+
+# %% List comprehension example 2
+seq = []
+for i in range(5):
+    seq.append(i * 2)
+print(seq)
+# output ➜ [0, 2, 4, 6, 8]
+
+seq = []
+seq = [i * 2 for i in range(5)]
+print(seq)
+# output ➜ [0, 2, 4, 6, 8]
+
+# %% List comprehension example 3
+seq = []
+for i in range(10):
+    if i % 2 == 1:
+        seq.append(i)
+print(seq)
+# output ➜ [1, 3, 5, 7, 9]
+
+seq = []
+seq = [i for i in range(10) if i % 2 == 1]
+print(seq)
+# output ➜ [1, 3, 5, 7, 9]
+
+# %% List comprehension example 4
+seq = []
+for i in range(1, 10):
+    if i % 2 == 0:
+        seq.append(i * 2)
+    else:
+        seq.append(i * 3)
+
+print(seq)
+# output ➜ [3, 4, 9, 8, 15, 12, 21, 16, 27]
+
+print("with ternary operator" )
+seq = []
+for i in range(1, 10):
+    seq.append(i * (2 if i % 2 == 0 else 3))
+
+print(seq)
+# output ➜ [3, 4, 9, 8, 15, 12, 21, 16, 27]
+
+print("with comprehension" )
+seq = []
+seq = [(i * (2 if i % 2 == 0 else 3)) for i in range(1, 10)]
+print(seq)
+# output ➜ [3, 4, 9, 8, 15, 12, 21, 16, 27]
+
+# %% List comprehension example 5
+list_x = ['a', 'b', 'c']
+list_y = ['1', '2', '3']
+
+seq = []
+for x in list_x:
+    for y in list_y:
+        seq.append(x + y)
+
+print(seq)
+# output ➜ ['a1', 'a2', 'a3', 'b1', 'b2', 'b3', 'c1', 'c2', 'c3']
+
+
+list_x = ['a', 'b', 'c']
+list_y = ['1', '2', '3']
+seq = [x + y for x in list_x for y in list_y]
+print(seq)
+# output ➜ ['a1', 'a2', 'a3', 'b1', 'b2', 'b3', 'c1', 'c2', 'c3']
+
+# %% List comprehension example 6
+matrix = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+]
+transposed = []
+for i in range(4):
+    tr = []
+    for row in matrix:
+        tr.append(row[i])
+    transposed.append(tr)
+
+print(transposed)
+# output ➜ [[1, 5, 9], [2, 6, 10], [3, 7, 11], [4, 8, 12]]
+
+print("diringkas" )
+matrix = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+]
+transposed = []
+for i in range(4):
+    transposed.append([row[i] for row in matrix])
+
+print(transposed)
+# output ➜ [[1, 5, 9], [2, 6, 10], [3, 7, 11], [4, 8, 12]]
+
+
+print("with comprehension" )
+matrix = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+]
+transposed = [[row[i] for row in matrix] for i in range(4)]
+print(transposed)
+# output ➜ [[1, 5, 9], [2, 6, 10], [3, 7, 11], [4, 8, 12]]
