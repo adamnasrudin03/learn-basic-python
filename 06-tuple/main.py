0f423883d72a05e4c60d551a8cbe3fbd3d97ab44
Adam Nasrudin <adamnasrudin858@gmail.com>
2024-06-20 16:40:33 +0700

    into tuple

---
+++ 06-tuple/main.py
@@ -0,0 +1,14 @@
+tuple_1 = (2, 3, 4, "hello python", False)
+
+print("data:", tuple_1)
+# output ➜ data: (2, 3, 4, "hello python", False)
+
+print("total elem:", len(tuple_1))
+# output ➜ total elem: 5
+
+tuple_1 = (2, 3, 4, 5)
+print("elem 0:", tuple_1[0])
+# output ➜ elem 0: 2
+
+print("elem 1:", tuple_1[1])
+# output ➜ elem 1: 3
